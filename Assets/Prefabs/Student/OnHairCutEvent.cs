3baa76cfe49ed4de0312934d072b03c56b1da3be
夜轮_NachtgeistW <nachtgeistw@protonmail.com>
2024-12-24 17:12:13 +0800

    Add hair split function

--- Assets/Prefabs/Student/OnHairCutEvent.cs
+++ Assets/Prefabs/Student/OnHairCutEvent.cs
@@ -8,6 +8,7 @@ namespace Game
     public class OnHairCutEvent : MonoBehaviour
     {
         [SerializeField] private Transform studentTransform;
+        [SerializeField] private Transform originalHairTransform;
 
         private void OnEnable()
         {
@@ -23,7 +24,7 @@ namespace Game
         {
             SplitHair(evt.cutPosition, evt.hair);
         }
-        
+
         private void SplitHair(Vector2 cutPoint, GameObject hairObject)
         {
             // Get the original sprite
@@ -35,21 +36,7 @@ namespace Game
             var bottomHalf = new Texture2D(originalTexture.width, originalTexture.height / 2);
 
             // Copy pixels to new textures
-            for (var x = 0; x < originalTexture.width; x++)
-            {
-                for (var y = 0; y < originalTexture.height; y++)
-                {
-                    var pixel = originalTexture.GetPixel(x, y);
-                    if (y < originalTexture.height / 2)
-                    {
-                        bottomHalf.SetPixel(x, y, pixel);
-                    }
-                    else
-                    {
-                        topHalf.SetPixel(x, y - originalTexture.height / 2, pixel);
-                    }
-                }
-            }
+            CopyPixelsToNewTexture();
 
             // Apply changes
             topHalf.Apply();
@@ -62,6 +49,26 @@ namespace Game
             // Destroy original hair
             //Destroy(gameObject);
             hairObject.SetActive(false);
+            return;
+
+            void CopyPixelsToNewTexture()
+            {
+                for (var x = 0; x < originalTexture.width; x++)
+                {
+                    for (var y = 0; y < originalTexture.height; y++)
+                    {
+                        var pixel = originalTexture.GetPixel(x, y);
+                        if (y < originalTexture.height / 2)
+                        {
+                            bottomHalf.SetPixel(x, y, pixel);
+                        }
+                        else
+                        {
+                            topHalf.SetPixel(x, y - originalTexture.height / 2, pixel);
+                        }
+                    }
+                }
+            }
         }
 
         private void CreateSplitPiece(Texture2D texture, bool isTop)
@@ -69,23 +76,40 @@ namespace Game
             var newPiece = new GameObject("HairPiece");
             var newPieceRenderer = newPiece.AddComponent<SpriteRenderer>();
             newPiece.transform.SetParent(studentTransform);
-    
+
+            // Get the original sprite's properties
+            SpriteRenderer originalRenderer = originalHairTransform.GetComponent<SpriteRenderer>();
+            Vector2 originalPivot = originalRenderer.sprite.pivot;
+            float pixelsPerUnit = originalRenderer.sprite.pixelsPerUnit;
+            
             // Create new sprite from texture
-            var newSprite = Sprite.Create(texture, 
-                new Rect(0, 0, texture.width, texture.height), 
-                new Vector2(0.5f, 0.5f));
-    
+            var newSprite = Sprite.Create(
+                texture,
+                new Rect(0, 0, texture.width, texture.height),
+                new Vector2(originalPivot.x / texture.width, originalPivot.y / texture.height), // Use original pivot ratio
+                pixelsPerUnit
+            );
+
             newPieceRenderer.sprite = newSprite;
-            newPieceRenderer.sortingLayerName = "Hair";
-    
-            // Position the new piece
-            newPiece.transform.position = transform.position + (isTop ? Vector3.up : Vector3.down) * texture.height / 2;
-    
+
+            // Match the original sprite's properties
+            newPieceRenderer.sortingOrder = originalRenderer.sortingOrder;
+            newPieceRenderer.sortingLayerID = originalRenderer.sortingLayerID;
+            
+            newPiece.transform.rotation = transform.rotation;
+            newPiece.transform.localScale = transform.localScale;
+
             if (isTop)
             {
+                newPiece.transform.position = transform.position;
                 // Add some physics effects
                 var rb = newPiece.AddComponent<Rigidbody2D>();
                 rb.AddForce(new Vector2(Random.Range(-100f, 100f), Random.Range(-50f, 50f)));
+            }
+            else
+            {
+                newPiece.transform.position =
+                    new Vector3(transform.position.x + 1, transform.position.y + 1.65f, transform.position.z);
             }
         }
     }
