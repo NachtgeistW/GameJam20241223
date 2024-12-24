9fab94d93773a6d7eaf2cf39ec1781206b4c895f
夜轮_NachtgeistW <nachtgeistw@protonmail.com>
2024-12-24 16:36:49 +0800

    将头发生成拆分到新的脚本里

---
+++ Assets/Prefabs/Student/OnHairCutEvent.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using Plutono.Util;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+namespace Game
+{
+    public class OnHairCutEvent : MonoBehaviour
+    {
+        [SerializeField] private Transform studentTransform;
+
+        private void OnEnable()
+        {
+            EventCenter.AddListener<HairCutEvent>(OnHairCut);
+        }
+
+        private void OnDisable()
+        {
+            EventCenter.RemoveListener<HairCutEvent>(OnHairCut);
+        }
+
+        private void OnHairCut(HairCutEvent evt)
+        {
+            SplitHair(evt.cutPosition, evt.hair);
+        }
+        
+        private void SplitHair(Vector2 cutPoint, GameObject hairObject)
+        {
+            // Get the original sprite
+            var originalSprite = hairObject.GetComponent<SpriteRenderer>();
+            var originalTexture = originalSprite.sprite.texture;
+
+            // Create two new sprites
+            var topHalf = new Texture2D(originalTexture.width, originalTexture.height / 2);
+            var bottomHalf = new Texture2D(originalTexture.width, originalTexture.height / 2);
+
+            // Copy pixels to new textures
+            for (var x = 0; x < originalTexture.width; x++)
+            {
+                for (var y = 0; y < originalTexture.height; y++)
+                {
+                    var pixel = originalTexture.GetPixel(x, y);
+                    if (y < originalTexture.height / 2)
+                    {
+                        bottomHalf.SetPixel(x, y, pixel);
+                    }
+                    else
+                    {
+                        topHalf.SetPixel(x, y - originalTexture.height / 2, pixel);
+                    }
+                }
+            }
+
+            // Apply changes
+            topHalf.Apply();
+            bottomHalf.Apply();
+
+            // Create new GameObjects with split sprites
+            CreateSplitPiece(topHalf, true);
+            CreateSplitPiece(bottomHalf, false);
+
+            // Destroy original hair
+            //Destroy(gameObject);
+            hairObject.SetActive(false);
+        }
+
+        private void CreateSplitPiece(Texture2D texture, bool isTop)
+        {
+            var newPiece = new GameObject("HairPiece");
+            var newPieceRenderer = newPiece.AddComponent<SpriteRenderer>();
+            newPiece.transform.SetParent(studentTransform);
+    
+            // Create new sprite from texture
+            var newSprite = Sprite.Create(texture, 
+                new Rect(0, 0, texture.width, texture.height), 
+                new Vector2(0.5f, 0.5f));
+    
+            newPieceRenderer.sprite = newSprite;
+            newPieceRenderer.sortingLayerName = "Hair";
+    
+            // Position the new piece
+            newPiece.transform.position = transform.position + (isTop ? Vector3.up : Vector3.down) * texture.height / 2;
+    
+            if (isTop)
+            {
+                // Add some physics effects
+                var rb = newPiece.AddComponent<Rigidbody2D>();
+                rb.AddForce(new Vector2(Random.Range(-100f, 100f), Random.Range(-50f, 50f)));
+            }
+        }
+    }
+}
--- Assets/Scripts/Game/HairCutter.cs
+++ Assets/Scripts/Game/HairCutter.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Plutono.Util;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.EventSystems;
@@ -6,6 +7,12 @@ using Random = UnityEngine.Random;
 
 namespace Game
 {
+    public struct HairCutEvent : IEvent
+    {
+        public Vector2 cutPosition;
+        public GameObject hair;
+    }
+
     public class HairCutter : MonoBehaviour
     {
         private bool isMousePressed = false;
@@ -33,13 +40,13 @@ namespace Game
             if (Input.GetMouseButtonUp(0))
             {
                 isMousePressed = false;
-                
+
                 var mousePos = Input.mousePosition;
                 mousePos.z = Mathf.Abs(cam.transform.position.z);
                 endCutPoint = cam.ScreenToWorldPoint(mousePos);
                 CheckCutting();
             }
-            
+
             DrawLine4Debug();
         }
 
@@ -47,7 +54,7 @@ namespace Game
         {
             var cutDirection = endCutPoint - startCutPoint;
             //Debug.Log($"{startCutPoint}, {endCutPoint}, {cutDirection}");
-            
+
             //TODO: what did this raycast do??
             var hit = Physics2D.Raycast(startCutPoint, cutDirection.normalized, cutDirection.magnitude);
             if (hit.collider)
@@ -55,78 +62,20 @@ namespace Game
                 Debug.Log(hit.collider.gameObject.name);
                 if (hit.collider.CompareTag("Hair"))
                 {
-                    SplitHair(hit.point, hit.collider.gameObject);
+                    EventCenter.Broadcast(new HairCutEvent
+                    {
+                        cutPosition = hit.point,
+                        hair = hit.collider.gameObject
+                    });
                 }
+
                 if (hit.collider.CompareTag("Body"))
                 {
                     Debug.Log("You cut the body!!!");
                 }
             }
         }
-        
-        private void SplitHair(Vector2 cutPoint, GameObject hairObject)
-        {
-            // Get the original sprite
-            var originalSprite = hairObject.GetComponent<SpriteRenderer>();
-            var originalTexture = originalSprite.sprite.texture;
 
-            // Create two new sprites
-            var topHalf = new Texture2D(originalTexture.width, originalTexture.height / 2);
-            var bottomHalf = new Texture2D(originalTexture.width, originalTexture.height / 2);
-
-            // Copy pixels to new textures
-            for (var x = 0; x < originalTexture.width; x++)
-            {
-                for (var y = 0; y < originalTexture.height; y++)
-                {
-                    var pixel = originalTexture.GetPixel(x, y);
-                    if (y < originalTexture.height / 2)
-                    {
-                        bottomHalf.SetPixel(x, y, pixel);
-                    }
-                    else
-                    {
-                        topHalf.SetPixel(x, y - originalTexture.height / 2, pixel);
-                    }
-                }
-            }
-
-            // Apply changes
-            topHalf.Apply();
-            bottomHalf.Apply();
-
-            // Create new GameObjects with split sprites
-            CreateSplitPiece(topHalf, true);
-            CreateSplitPiece(bottomHalf, false);
-
-            // Destroy original hair
-            //Destroy(gameObject);
-            hairObject.SetActive(false);
-        }
-
-        private void CreateSplitPiece(Texture2D texture, bool isTop)
-        {
-            var newPiece = new GameObject("HairPiece");
-            var newPieceRenderer = newPiece.AddComponent<SpriteRenderer>();
-    
-            // Create new sprite from texture
-            var newSprite = Sprite.Create(texture, 
-                new Rect(0, 0, texture.width, texture.height), 
-                new Vector2(0.5f, 0.5f));
-    
-            newPieceRenderer.sprite = newSprite;
-    
-            // Position the new piece
-            newPiece.transform.position = transform.position + (isTop ? Vector3.up : Vector3.down) * texture.height / 2;
-    
-            if (isTop)
-            {
-                // Add some physics effects
-                var rb = newPiece.AddComponent<Rigidbody2D>();
-                rb.AddForce(new Vector2(Random.Range(-100f, 100f), Random.Range(-50f, 50f)));
-            }
-        }
-        
         private void DrawLine4Debug()
         {
             if (isMousePressed)
