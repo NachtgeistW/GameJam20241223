c969942004e00a45ae2c09fb30c9a9144f8cda21
夜轮_NachtgeistW <nachtgeistw@protonmail.com>
2024-12-24 15:22:47 +0800

    Add mouse move pos detect

---
+++ Assets/Scripts/Game/HairCutter.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace Game
+{
+    public class HairCutter : MonoBehaviour
+    {
+        private bool isMousePressed = false;
+        private Vector2 startCutPoint;
+        private Vector2 endCutPoint;
+
+        [SerializeField] private Camera cam;
+
+        private void Start()
+        {
+            cam = FindObjectOfType<Camera>();
+        }
+
+        private void Update()
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                isMousePressed = true;
+
+                var mousePos = Input.mousePosition;
+                mousePos.z = Mathf.Abs(cam.transform.position.z);
+                startCutPoint = cam.ScreenToWorldPoint(mousePos);
+            }
+
+            if (Input.GetMouseButtonUp(0))
+            {
+                isMousePressed = false;
+                
+                var mousePos = Input.mousePosition;
+                mousePos.z = Mathf.Abs(cam.transform.position.z);
+                endCutPoint = cam.ScreenToWorldPoint(mousePos);
+                CheckHairCut();
+            }
+            
+            DrawLine4Debug();
+        }
+
+        private void CheckHairCut()
+        {
+            var cutDirection = endCutPoint - startCutPoint;
+            Debug.Log($"{startCutPoint}, {endCutPoint}, {cutDirection}");
+        }
+        
+        private void DrawLine4Debug()
+        {
+            if (isMousePressed)
+            {
+                var mousePos = Input.mousePosition;
+                mousePos.z = Mathf.Abs(cam.transform.position.z);
+                var worldPos = cam.ScreenToWorldPoint(mousePos);
+                Debug.DrawLine(startCutPoint, worldPos, Color.red);
+            }
+        }
+    }
+}
