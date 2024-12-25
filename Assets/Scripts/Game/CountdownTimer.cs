5679bdc585f3d9c88d66b7191b945cb3b0801cf0
夜轮_NachtgeistW <nachtgeistw@protonmail.com>
2024-12-25 21:14:13 +0800

    Add timer

---
+++ Assets/Scripts/Game/CountdownTimer.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Game
+{
+    public class CountdownTimer : MonoBehaviour
+    {
+        [SerializeField] private Text countdownText;
+        
+        private bool takingAway = false;
+        [SerializeField] private float secondsLeft = 30f;
+
+        private void Start()
+        {
+            countdownText.text = $"TIME: {secondsLeft}s";
+        }
+
+        private void Update()
+        {
+            if (!takingAway && secondsLeft > 0f)
+            {
+                StartCoroutine(TimerTake());
+            }
+        }
+
+        private IEnumerator TimerTake()
+        {
+            takingAway = true;
+            yield return new WaitForSeconds(1);
+            secondsLeft -= 1;
+            countdownText.text = $"TIME: {secondsLeft}s";
+            takingAway = false;
+        }
+    }
+}
